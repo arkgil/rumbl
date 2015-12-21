30b2c994f6c21870cefe842645a824e934dbb078
barcadius <arkadiusz.gil95@gmail.com>
2015-12-21 23:16:55 +0100

    Create watch controller and templates

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule Rumbl.Router do
   scope "/", Rumbl do
     pipe_through :browser # Use the default browser stack
     get "/", PageController, :index
+    get "/watch/:id", WatchController, :show
     resources "/sessions", SessionController, only: [:new, :create, :delete]
     resources "/users", UserController, only: [:index, :show, :new, :create]
   end

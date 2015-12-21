aecc391d5c126e7a674f6015e85da0eeb6f5d5ed
barcadius <arkadiusz.gil95@gmail.com>
2015-12-21 09:09:15 +0100

    Create static User repo

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,3 +1,22 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
+  @moduledoc """
+    In memory repository
+  """
+
+  def all(Rumbl.User) do
+    [%Rumbl.User{id: "1", name: "Arek", username: "barcadius", password: "12345678"},
+     %Rumbl.User{id: "2", name: "Emi", username: "emikis", password: "12345678"},
+     %Rumbl.User{id: "3", name: "Fgt", username: "fgt123", password: "12345678"}]
+  end
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
+  end
 end
